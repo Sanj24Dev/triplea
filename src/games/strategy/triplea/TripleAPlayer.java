Sanj24Dev/triplea

commit 9501cccdbbeec3a4940dee8532b143ad5febe7c8

Remove DefaultSoundChannel playLocal sound method, replace it with the one liner ClipPlayer method is called by that method.

src/games/strategy/triplea/TripleAPlayer.java
@@ -24,6 +24,7 @@ import games.strategy.engine.data.Territory;
 import games.strategy.engine.data.Unit;
 import games.strategy.engine.gamePlayer.IGamePlayer;
 import games.strategy.net.GUID;
+import games.strategy.sound.ClipPlayer;
 import games.strategy.sound.DefaultSoundChannel;
 import games.strategy.sound.SoundPath;
 import games.strategy.triplea.attatchments.PlayerAttachment;
@@ -272,9 +273,8 @@ public class TripleAPlayer extends AbstractHumanPlayer<TripleAFrame>implements I
 
 
     final PlayerID id = getPlayerID();
-    // play a sound for this phase
     if (!m_soundPlayedAlreadyTechnology) {
-      DefaultSoundChannel.playSoundOnLocalMachine(SoundPath.CLIP_PHASE_TECHNOLOGY, id.getName());
+      ClipPlayer.play(SoundPath.CLIP_PHASE_TECHNOLOGY, id.getName());
       m_soundPlayedAlreadyTechnology = true;
     }
     final TechRoll techRoll = m_ui.getTechRolls(id);
@@ -307,13 +307,9 @@ public class TripleAPlayer extends AbstractHumanPlayer<TripleAFrame>implements I
     }
 
     final PlayerID id = getPlayerID();
-    // play a sound for this phase
-    if (nonCombat && !m_soundPlayedAlreadyNonCombatMove) {
-      DefaultSoundChannel.playSoundOnLocalMachine(SoundPath.CLIP_PHASE_MOVE_NONCOMBAT, id.getName());
+    if ((nonCombat && !m_soundPlayedAlreadyNonCombatMove) || (!nonCombat && !m_soundPlayedAlreadyCombatMove)) {
+      ClipPlayer.play(SoundPath.CLIP_PHASE_MOVE_NONCOMBAT, id.getName());
       m_soundPlayedAlreadyNonCombatMove = true;
-    } else if (!nonCombat && !m_soundPlayedAlreadyCombatMove) {
-      DefaultSoundChannel.playSoundOnLocalMachine(SoundPath.CLIP_PHASE_MOVE_COMBAT, id.getName());
-      m_soundPlayedAlreadyCombatMove = true;
     }
     final MoveDescription moveDescription = m_ui.getMove(id, getPlayerBridge(), nonCombat, stepName);
     if (moveDescription == null) {
@@ -393,7 +389,7 @@ public class TripleAPlayer extends AbstractHumanPlayer<TripleAFrame>implements I
     final PlayerID id = getPlayerID();
     // play a sound for this phase
     if (!bid && !m_soundPlayedAlreadyPurchase) {
-      DefaultSoundChannel.playSoundOnLocalMachine(SoundPath.CLIP_PHASE_PURCHASE, id.getName());
+      ClipPlayer.play(SoundPath.CLIP_PHASE_PURCHASE, id.getName());
       m_soundPlayedAlreadyPurchase = true;
     }
     // Check if any factories need to be repaired
@@ -490,9 +486,8 @@ public class TripleAPlayer extends AbstractHumanPlayer<TripleAFrame>implements I
         }
         return;
       }
-      // play a sound for this phase
       if (!m_soundPlayedAlreadyBattle) {
-        DefaultSoundChannel.playSoundOnLocalMachine(SoundPath.CLIP_PHASE_BATTLE, id.getName());
+        ClipPlayer.play(SoundPath.CLIP_PHASE_BATTLE, id.getName());
         m_soundPlayedAlreadyBattle = true;
       }
       final FightBattleDetails details = m_ui.getBattle(id, battles.getBattles());
@@ -525,14 +520,9 @@ public class TripleAPlayer extends AbstractHumanPlayer<TripleAFrame>implements I
       e.printStackTrace();
       throw new IllegalStateException(errorContext, e);
     }
-    /*
-     * if (!placeDel.stuffToDoInThisDelegate())
-     * return;
-     */
     while (true) {
-      // play a sound for this phase
       if (!m_soundPlayedAlreadyPlacement) {
-        DefaultSoundChannel.playSoundOnLocalMachine(SoundPath.CLIP_PHASE_PLACEMENT, id.getName());
+        ClipPlayer.play(SoundPath.CLIP_PHASE_PLACEMENT, id.getName());
         m_soundPlayedAlreadyPlacement = true;
       }
       final PlaceData placeData = m_ui.waitForPlace(id, bid, getPlayerBridge());
@@ -572,7 +562,7 @@ public class TripleAPlayer extends AbstractHumanPlayer<TripleAFrame>implements I
     if (!m_soundPlayedAlreadyEndTurn && TerritoryAttachment.doWeHaveEnoughCapitalsToProduce(getPlayerID(), data)) {
       // do not play if we are reloading a savegame from pbem (gets annoying)
       if (!endTurnDelegate.getHasPostedTurnSummary()) {
-        DefaultSoundChannel.playSoundOnLocalMachine(SoundPath.CLIP_PHASE_END_TURN, getPlayerID().getName());
+        ClipPlayer.play(SoundPath.CLIP_PHASE_END_TURN, getPlayerID().getName());
       }
       m_soundPlayedAlreadyEndTurn = true;
     }

src/games/strategy/engine/chat/Chat.java
@@ -16,6 +16,7 @@ import games.strategy.engine.message.RemoteName;
 import games.strategy.net.IMessenger;
 import games.strategy.net.INode;
 import games.strategy.net.Messengers;
+import games.strategy.sound.ClipPlayer;
 import games.strategy.sound.DefaultSoundChannel;
 import games.strategy.sound.SoundPath;
 import games.strategy.util.Tuple;
@@ -332,7 +333,7 @@ public class Chat {
         for (final IChatListener listener : m_listeners) {
           listener.addStatusMessage(node.getName() + " has joined");
           if (m_chatSoundProfile == CHAT_SOUND_PROFILE.GAME_CHATROOM) {
-            DefaultSoundChannel.playSoundOnLocalMachine(SoundPath.CLIP_CHAT_JOIN_GAME, null);
+            ClipPlayer.play(SoundPath.CLIP_CHAT_JOIN_GAME);
           }
         }
       }

src/games/strategy/engine/chat/ChatMessagePanel.java
@@ -31,6 +31,7 @@ import javax.swing.text.StyleConstants;
 
 import games.strategy.net.INode;
 import games.strategy.net.ServerMessenger;
+import games.strategy.sound.ClipPlayer;
 import games.strategy.sound.DefaultSoundChannel;
 import games.strategy.sound.SoundPath;
 
@@ -248,10 +249,9 @@ public class ChatMessagePanel extends JPanel implements IChatListener {
             scrollModel.setValue(scrollModel.getMaximum());
           }
         });
-        DefaultSoundChannel.playSoundOnLocalMachine(sound, null);
+        ClipPlayer.play(sound);
       }
     };
-    // invoke in the swing event thread
     if (SwingUtilities.isEventDispatchThread()) {
       runner.run();
     } else {

src/games/strategy/engine/chat/HeadlessChat.java
@@ -16,6 +16,7 @@ import games.strategy.engine.message.IRemoteMessenger;
 import games.strategy.net.IMessenger;
 import games.strategy.net.INode;
 import games.strategy.net.ServerMessenger;
+import games.strategy.sound.ClipPlayer;
 import games.strategy.sound.DefaultSoundChannel;
 import games.strategy.sound.SoundPath;
 
@@ -184,7 +185,7 @@ public class HeadlessChat implements IChatListener, IChatPanel {
           return;
         }
         addChatMessage(message, from, thirdperson);
-        DefaultSoundChannel.playSoundOnLocalMachine(sound, null);
+        ClipPlayer.play(sound);
       }
     });
     t.start();

src/games/strategy/sound/ClipPlayer.java
@@ -276,13 +276,17 @@ public class ClipPlayer {
     return SoundPath.getSoundOptions(sounds);
   }
 
+  
+  public static void play(final String clipName) {
+    play(clipName,null);
+  }
   /**
    * @param clipName
    *        String - the file name of the clip
    * @param subFolder
    *        String - the name of the player, or null
    */
-  static void play(final String clipName, final String subFolder) {
+  public static void play(final String clipName, final String subFolder) {
     getInstance().playClip(clipName, subFolder);
   }
 

src/games/strategy/sound/DefaultSoundChannel.java
@@ -21,27 +21,10 @@ public class DefaultSoundChannel implements ISound {
     m_localPlayers = localPlayers;
   }
 
-  /**
-   * Plays a sound clip on this local machine.
-   * You will want to call this from UI elements (because all users have these), and not call it from delegates (because
-   * only the host has
-   * these).
-   *
-   * @param clipName
-   *        the name of the sound clip to play, found in SoundPath.java
-   * @param subFolder
-   *        the name of the player nation who's sound we want to play (ie: russians infantry might make different sounds
-   *        from german
-   *        infantry, etc). Can be null.
-   */
-  public static void playSoundOnLocalMachine(final String clipName, final String subFolder) {
-    ClipPlayer.play(clipName, subFolder);
-  }
-
 
   @Override
   public void playSoundForAll(final String clipName, final String subFolder) {
-    playSoundOnLocalMachine(clipName, subFolder);
+    ClipPlayer.play(clipName, subFolder);
   }
 
   @Override
@@ -69,7 +52,7 @@ public class DefaultSoundChannel implements ISound {
       isPlaying = true;
     }
     if (isPlaying) {
-      playSoundOnLocalMachine(clipName, subFolder);
+      ClipPlayer.play(clipName, subFolder);
     }
   }
 

src/games/strategy/triplea/TripleA.java
@@ -24,6 +24,7 @@ import games.strategy.engine.framework.headlessGameServer.HeadlessGameServerUI;
 import games.strategy.engine.gamePlayer.IGamePlayer;
 import games.strategy.engine.message.IChannelSubscribor;
 import games.strategy.engine.message.IRemote;
+import games.strategy.sound.ClipPlayer;
 import games.strategy.sound.DefaultSoundChannel;
 import games.strategy.sound.DummySoundChannel;
 import games.strategy.sound.ISound;
@@ -143,7 +144,7 @@ public class TripleA extends AbstractGameLoader implements IGameLoader {
             game.addSoundChannel(m_soundChannel);
             frame.setSize(700, 400);
             frame.setVisible(true);
-            DefaultSoundChannel.playSoundOnLocalMachine(SoundPath.CLIP_GAME_START, null);
+            ClipPlayer.play(SoundPath.CLIP_GAME_START);
             connectPlayers(players, frame);
             SwingUtilities.invokeLater(new Runnable() {
               @Override

src/games/strategy/triplea/ui/PoliticsPanel.java
@@ -29,6 +29,7 @@ import games.strategy.engine.data.GameData;
 import games.strategy.engine.data.PlayerID;
 import games.strategy.engine.data.RelationshipType;
 import games.strategy.engine.data.RelationshipTypeList;
+import games.strategy.sound.ClipPlayer;
 import games.strategy.sound.DefaultSoundChannel;
 import games.strategy.sound.SoundPath;
 import games.strategy.triplea.attatchments.PoliticalActionAttachment;
@@ -103,7 +104,7 @@ public class PoliticsPanel extends ActionPanel {
       return null;
     } else {
       if (m_firstRun) {
-        DefaultSoundChannel.playSoundOnLocalMachine(SoundPath.CLIP_PHASE_POLITICS, getCurrentPlayer().getName());
+        ClipPlayer.play(SoundPath.CLIP_PHASE_POLITICS, getCurrentPlayer().getName());
       }
       SwingUtilities.invokeLater(new Runnable() {
         @Override

commit 39d0431515b3f65a4a7c7625da43c35bf71d0f9d

ClipPlayer.java - Fix sound clip error introduced in 9501ccc

src/games/strategy/triplea/TripleAPlayer.java
@@ -313,7 +313,7 @@ public class TripleAPlayer extends AbstractHumanPlayer<TripleAFrame> implements 
     }
 
     if (!nonCombat && !m_soundPlayedAlreadyCombatMove) {
-      ClipPlayer.play(SoundPath.CLIP_PHASE_MOVE_NONCOMBAT, id);
+      ClipPlayer.play(SoundPath.CLIP_PHASE_MOVE_COMBAT, id);
       m_soundPlayedAlreadyCombatMove = true;
     }
 