Sanj24Dev/triplea

commit 0bfce5b92fed12aaaeed0c5d8eb4d624a5c172e3

Make unitImageFactory.getIcon() always return an icon. (#10515)
* Make unitImageFactory.getIcon() always return an icon.

Make unitImageFactory.getIcon() always return an icon, by making use of the "missing unit" icon for missing units. This reduces complexity by callers not having to have extra logic to handle missing icons and brings consistency with other places that show images.

Also some small clean ups in files being changed.

* Remove unused logging annotation and improve comments.

game-app/game-core/src/main/java/games/strategy/triplea/image/UnitImageFactory.java
@@ -356,20 +356,10 @@ public class UnitImageFactory {
     return highlightedImage;
   }
 
-  /** Return a icon image for a unit. */
-  public Optional<ImageIcon> getIcon(final ImageKey imageKey) {
+  /** Return an icon image for a unit. */
+  public ImageIcon getIcon(final ImageKey imageKey) {
     final String fullName = imageKey.getFullName();
-    if (icons.containsKey(fullName)) {
-      return Optional.of(icons.get(fullName));
-    }
-    final Optional<Image> image = getTransformedImage(imageKey);
-    if (image.isEmpty()) {
-      return Optional.empty();
-    }
-
-    final ImageIcon icon = new ImageIcon(image.get());
-    icons.put(fullName, icon);
-    return Optional.of(icon);
+    return icons.computeIfAbsent(fullName, key -> new ImageIcon(getImage(imageKey)));
   }
 
   public Dimension getImageDimensions(final ImageKey imageKey) {

game-app/game-core/src/main/java/games/strategy/triplea/odds/calculator/OrderOfLossesInputPanel.java
@@ -20,7 +20,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -278,20 +277,15 @@ class OrderOfLossesInputPanel extends JPanel {
                 + TooltipProperties.getInstance()
                     .getTooltip(category.getType(), category.getOwner())
                 + "</html>";
-        final Optional<ImageIcon> img =
-            uiContext.getUnitImageFactory().getIcon(ImageKey.of(category));
-        if (img.isPresent()) {
-          final JButton button = new JButton(img.get());
-          button.setToolTipText(toolTipText);
-          button.addActionListener(
-              e ->
-                  textField.setText(
-                      (textField.getText().length() > 0
-                              ? (textField.getText() + OOL_SEPARATOR)
-                              : "")
-                          + unitName));
-          panel.add(button);
-        }
+        final ImageIcon img = uiContext.getUnitImageFactory().getIcon(ImageKey.of(category));
+        final JButton button = new JButton(img);
+        button.setToolTipText(toolTipText);
+        button.addActionListener(
+            e ->
+                textField.setText(
+                    (textField.getText().length() > 0 ? (textField.getText() + OOL_SEPARATOR) : "")
+                        + unitName));
+        panel.add(button);
         typesUsed.add(category.getType());
       }
     }

game-app/game-core/src/main/java/games/strategy/triplea/odds/calculator/UnitPanel.java
@@ -44,12 +44,7 @@ public class UnitPanel extends JPanel {
     setCount(category.getUnits().size());
     setLayout(new GridBagLayout());
 
-    final JLabel label =
-        uiContext
-            .getUnitImageFactory()
-            .getIcon(ImageKey.of(category))
-            .map(JLabel::new)
-            .orElseGet(JLabel::new);
+    final JLabel label = new JLabel(uiContext.getUnitImageFactory().getIcon(ImageKey.of(category)));
     label.setToolTipText(toolTipText);
     add(
         label,

game-app/game-core/src/main/java/games/strategy/triplea/ui/AbstractUndoableMovesPanel.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
@@ -122,15 +121,13 @@ public abstract class AbstractUndoableMovesPanel extends JPanel {
     final Collection<UnitCategory> unitCategories = UnitSeparator.categorize(move.getUnits());
     final Dimension buttonSize = new Dimension(80, 22);
     for (final UnitCategory category : unitCategories) {
-      final Optional<ImageIcon> icon =
+      final ImageIcon icon =
           movePanel.getMap().getUiContext().getUnitImageFactory().getIcon(ImageKey.of(category));
-      if (icon.isPresent()) {
-        final JLabel label =
-            new JLabel("x" + category.getUnits().size() + " ", icon.get(), SwingConstants.LEFT);
-        unitsBox.add(label);
-        MapUnitTooltipManager.setUnitTooltip(
-            label, category.getType(), category.getOwner(), category.getUnits().size());
-      }
+      final JLabel label =
+          new JLabel("x" + category.getUnits().size() + " ", icon, SwingConstants.LEFT);
+      unitsBox.add(label);
+      MapUnitTooltipManager.setUnitTooltip(
+          label, category.getType(), category.getOwner(), category.getUnits().size());
     }
     unitsBox.add(Box.createHorizontalGlue());
     final JLabel text = new JLabel(move.getMoveLabel());

game-app/game-core/src/main/java/games/strategy/triplea/ui/BattleDisplay.java
@@ -222,11 +222,9 @@ public class BattleDisplay extends JPanel {
       }
     }
     final Map<Unit, Collection<Unit>> dependentsMap;
-    gameData.acquireReadLock();
-    try {
+
+    try (GameData.Unlocker ignored = gameData.acquireReadLock()) {
       dependentsMap = CasualtyUtil.getDependents(killedUnits);
-    } finally {
-      gameData.releaseReadLock();
     }
     final Collection<Unit> dependentUnitsReturned = new ArrayList<>();
     for (final Collection<Unit> dependentCollection : dependentsMap.values()) {
@@ -779,7 +777,7 @@ public class BattleDisplay extends JPanel {
         final Iterable<UnitCategory> categoryIter, final boolean damaged, final boolean disabled) {
       for (final UnitCategory category : categoryIter) {
         final JPanel panel = new JPanel();
-        final Optional<ImageIcon> unitImage =
+        final ImageIcon unitImage =
             uiContext
                 .getUnitImageFactory()
                 .getIcon(
@@ -789,7 +787,7 @@ public class BattleDisplay extends JPanel {
                         .damaged(damaged && category.hasDamageOrBombingUnitDamage())
                         .disabled(disabled && category.getDisabled())
                         .build());
-        final JLabel unit = unitImage.map(JLabel::new).orElseGet(JLabel::new);
+        final JLabel unit = new JLabel(unitImage);
         panel.add(unit);
         // Add a tooltip, with a count of 1 so that the tooltip doesn't have a number label (so it
         // won't get out of date

game-app/game-core/src/main/java/games/strategy/triplea/ui/BattleModel.java
@@ -18,7 +18,6 @@ import games.strategy.triplea.util.UnitSeparator;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.table.DefaultTableModel;
@@ -101,8 +100,7 @@ final class BattleModel extends DefaultTableModel {
     final List<Unit> units = new ArrayList<>(this.units);
     final TotalPowerAndTotalRolls unitPowerAndRollsMap;
     final boolean isAirPreBattleOrPreRaid = battleType.isAirBattle();
-    gameData.acquireReadLock();
-    try {
+    try (GameData.Unlocker ignored = gameData.acquireReadLock()) {
       final CombatValue combatValue;
       if (isAirPreBattleOrPreRaid) {
         combatValue =
@@ -125,8 +123,6 @@ final class BattleModel extends DefaultTableModel {
                 .build();
       }
       unitPowerAndRollsMap = PowerStrengthAndRolls.build(units, combatValue);
-    } finally {
-      gameData.releaseReadLock();
     }
     final Collection<UnitCategory> unitCategories = UnitSeparator.categorize(units);
     for (final UnitCategory category : unitCategories) {
@@ -173,7 +169,7 @@ final class BattleModel extends DefaultTableModel {
     private GamePlayer player;
     private UnitType unitType;
     private int count;
-    private Optional<ImageIcon> icon;
+    private ImageIcon icon;
 
     private TableData() {}
 
@@ -182,7 +178,7 @@ final class BattleModel extends DefaultTableModel {
       this.player = imageKey.getPlayer();
       this.count = count;
       this.unitType = imageKey.getType();
-      icon = uiContext.getUnitImageFactory().getIcon(imageKey);
+      this.icon = uiContext.getUnitImageFactory().getIcon(imageKey);
     }
 
     void updateStamp(final JLabel stamp) {
@@ -192,7 +188,7 @@ final class BattleModel extends DefaultTableModel {
         stamp.setToolTipText(null);
       } else {
         stamp.setText("x" + count);
-        icon.ifPresent(stamp::setIcon);
+        stamp.setIcon(icon);
         MapUnitTooltipManager.setUnitTooltip(stamp, unitType, player, count);
       }
     }

game-app/game-core/src/main/java/games/strategy/triplea/ui/ProductionPanel.java
@@ -113,8 +113,7 @@ class ProductionPanel extends JPanel {
 
   protected void initRules(
       final GamePlayer player, final IntegerMap<ProductionRule> initialPurchase) {
-    this.data.acquireReadLock();
-    try {
+    try (GameData.Unlocker ignored = this.data.acquireReadLock()) {
       gamePlayer = player;
       for (final ProductionRule productionRule : player.getProductionFrontier()) {
         final Rule rule = new Rule(productionRule, player);
@@ -122,8 +121,6 @@ class ProductionPanel extends JPanel {
         rule.setQuantity(initialQuantity);
         rules.add(rule);
       }
-    } finally {
-      this.data.releaseReadLock();
     }
   }
 
@@ -277,16 +274,13 @@ class ProductionPanel extends JPanel {
       Preconditions.checkState(gamePlayer != null, "bid was true while id is null");
       // TODO bid only allows you to add PU's to the bid... maybe upgrading Bids so multiple
       // resources can be given?
-      final String propertyName = gamePlayer.getName() + " bid";
-      final int bid = data.getProperties().get(propertyName, 0);
-      final ResourceCollection bidCollection = new ResourceCollection(data);
-      data.acquireReadLock();
-      try {
+      try (GameData.Unlocker ignored = data.acquireReadLock()) {
+        final String propertyName = gamePlayer.getName() + " bid";
+        final int bid = data.getProperties().get(propertyName, 0);
+        final ResourceCollection bidCollection = new ResourceCollection(data);
         bidCollection.addResource(data.getResourceList().getResource(Constants.PUS), bid);
-      } finally {
-        data.releaseReadLock();
+        return bidCollection;
       }
-      return bidCollection;
     }
 
     return (gamePlayer == null || gamePlayer.isNull())
@@ -331,7 +325,7 @@ class ProductionPanel extends JPanel {
           name.getFont().deriveFont(Map.of(TextAttribute.WEIGHT, TextAttribute.WEIGHT_BOLD)));
       final JLabel info = new JLabel("  ");
       final Color defaultForegroundLabelColor = name.getForeground();
-      Optional<ImageIcon> icon = Optional.empty();
+      ImageIcon icon = null;
       final StringBuilder tooltip = new StringBuilder();
       final Set<NamedAttachable> results = new HashSet<>(rule.getResults().keySet());
       final Iterator<NamedAttachable> iter = results.iterator();
@@ -363,7 +357,7 @@ class ProductionPanel extends JPanel {
           }
         } else if (resourceOrUnit instanceof Resource) {
           final Resource resource = (Resource) resourceOrUnit;
-          icon = Optional.of(uiContext.getResourceImageFactory().getLargeIcon(resource.getName()));
+          icon = uiContext.getResourceImageFactory().getLargeIcon(resource.getName());
           info.setText("resource");
           tooltip.append(resource.getName()).append(": resource");
           name.setText(resource.getName());
@@ -416,7 +410,7 @@ class ProductionPanel extends JPanel {
                 0));
       }
       final JPanel label = new JPanel();
-      icon.ifPresent(imageIcon -> label.add(new JLabel(imageIcon)));
+      Optional.ofNullable(icon).ifPresent(imageIcon -> label.add(new JLabel(imageIcon)));
       label.add(costPanel);
 
       final ScrollableTextField textField = new ScrollableTextField(0, Integer.MAX_VALUE);

game-app/game-core/src/main/java/games/strategy/triplea/ui/ProductionRepairPanel.java
@@ -27,6 +27,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
 import javax.swing.Action;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -301,18 +302,14 @@ class ProductionRepairPanel extends JPanel {
             "Rule unit type "
                 + type.getName()
                 + " does not match "
-                + repairUnit.toString()
+                + repairUnit
                 + ".  Please make sure your maps are up to date!");
       }
       repairResults = rule.getResults().getInt(type);
       final String text = "<html> x " + ResourceCollection.toStringForHtml(cost, data) + "</html>";
 
-      final JLabel label =
-          uiContext
-              .getUnitImageFactory()
-              .getIcon(ImageKey.of(repairUnit))
-              .map(imageIcon -> new JLabel(text, imageIcon, SwingConstants.LEFT))
-              .orElseGet(() -> new JLabel(text, SwingConstants.LEFT));
+      final ImageIcon icon = uiContext.getUnitImageFactory().getIcon(ImageKey.of(repairUnit));
+      final JLabel label = new JLabel(text, icon, SwingConstants.LEFT);
       final JLabel info = new JLabel(territoryUnitIsIn.getName());
       maxRepairAmount = repairUnit.getHowMuchCanThisUnitBeRepaired(territoryUnitIsIn);
       final JLabel remaining = new JLabel("Damage left to repair: " + maxRepairAmount);

commit 69999eadd77bb297f604e3095bd91f052dd2317d

Image fix (#12507)
Fixes: #12500

Issue is difference between: getImage(imageKey) vs getTransformedImage(imageKey)
getImage returns a scaled image, getTransformedImage returns an unscaled image.

Issue was introduced in: 0bfce5b, there was a mistake to swap 'getTransformedImage' with 'getImage'. Without the clarification that one returns a scaled image and the other does not, it really does look they ought to return the same result. The multiple layers of caching also makes this confusing. It's very surprising for the 'image icon' cache to store unscaled images while the 'images' cache stores scaled images (one would expect both of them to store scaled images).

The problem with the purchase screen is that the scaled images were being used, rather than the 100% scale images.

game-app/game-core/src/main/java/games/strategy/triplea/image/UnitImageFactory.java
@@ -57,9 +57,9 @@ public class UnitImageFactory {
 
   private final int unitCounterOffsetWidth;
   private final int unitCounterOffsetHeight;
-  // maps Point -> image
+  // maps Point -> (scaled) image
   private final Map<ImageKey, Image> images = new HashMap<>();
-  // maps Point -> Icon
+  // maps Point -> (unscaled) Icon
   private final Map<String, ImageIcon> icons = new HashMap<>();
   // Temporary colorized image files used for URLs for html views (e.g. unit stats table).
   private final Map<ImageKey, URL> colorizedTempFiles = new HashMap<>();
@@ -227,8 +227,8 @@ public class UnitImageFactory {
   }
 
   /**
-   * Return the appropriate unit image. If an image cannot be found, a placeholder 'no-image' image
-   * is returned.
+   * Return the appropriate scaled unit image. If an image cannot be found, a placeholder 'no-image'
+   * image is returned.
    */
   public Image getImage(final ImageKey imageKey) {
     return Optional.ofNullable(images.get(imageKey))
@@ -253,20 +253,21 @@ public class UnitImageFactory {
                           images.put(imageKey, scaledImage);
                           return scaledImage;
                         }))
-        .orElseGet(
-            () -> {
-              BufferedImage image =
-                  resourceLoader.getImageOrThrow(FILE_NAME_BASE + "missing_unit_image.png");
-              Color playerColor = mapData.getPlayerColor(imageKey.getPlayer().getName());
-              ImageTransformer.colorize(playerColor, image);
+        .orElseGet(() -> createNoImageImage(imageKey));
+  }
 
-              Graphics graphics = image.getGraphics();
-              Font font = graphics.getFont();
-              graphics.setFont(font.deriveFont(8.0f));
-              graphics.setColor(Color.LIGHT_GRAY);
-              graphics.drawString(imageKey.getBaseImageName(), 5, 28);
-              return image;
-            });
+  private Image createNoImageImage(ImageKey imageKey) {
+    BufferedImage image = resourceLoader.getImageOrThrow(FILE_NAME_BASE + "missing_unit_image.png");
+    Color playerColor = mapData.getPlayerColor(imageKey.getPlayer().getName());
+    ImageTransformer.colorize(playerColor, image);
+
+    Graphics graphics = image.getGraphics();
+    Font font = graphics.getFont();
+    graphics.setFont(font.deriveFont(8.0f));
+    graphics.setColor(Color.LIGHT_GRAY);
+    graphics.drawString(imageKey.getBaseImageName(), 5, 28);
+    images.put(imageKey, image);
+    return image;
   }
 
   public Optional<URL> getBaseImageUrl(final ImageKey imageKey) {
@@ -374,10 +375,14 @@ public class UnitImageFactory {
     return highlightedImage;
   }
 
-  /** Return an icon image for a unit. */
+  /** Return an _unscaled_ icon image for a unit. */
   public ImageIcon getIcon(final ImageKey imageKey) {
     final String fullName = imageKey.getFullName();
-    return icons.computeIfAbsent(fullName, key -> new ImageIcon(getImage(imageKey)));
+    return icons.computeIfAbsent(
+        fullName,
+        name ->
+            new ImageIcon(
+                getTransformedImage(imageKey).orElseGet(() -> createNoImageImage(imageKey))));
   }
 
   public Dimension getImageDimensions(final ImageKey imageKey) {